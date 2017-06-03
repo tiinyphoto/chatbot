10a2919ea048e4bd1d12854dc5181455455ac3b0
tin537 <arekorlab@gmail.com>
2017-06-03 22:44:36 +0700

    t0003 qurey_fin

--- train/qurey.py
+++ train/qurey.py
@@ -1,5 +1,7 @@
-jj = {"ff":"tin"}
-dict_target = {"tin":jj,'2':3}
+wifidic = {"เข้าไม่ได้":"ปิดเปิดใหม่"}
+landic ={"เข้าไม่ได้":"ถอดเสียบใหม่"}
+internetdic = {"wifi":wifidic,'lan':landic}
+dict_target = {"internet":internetdic,"wifi":wifidic,'lan':landic}
 def dd(check, target):
     if target is None:
         return False
@@ -7,19 +9,21 @@ def dd(check, target):
         if check in target:
             return target[check]
     elif isinstance(target, str):
-
         return target
-
-def check(word):
-    d = dd(word, dict_target)
+def check(word,target):
+    d = dd(word, target)
     while True:
+        temp = d
+        if d is False:
+            return "กรุณาติดต่อเจ้าหน้าที่"
         if d is None:
-            arg2 = input("need more:")
+            arg2 = input("ขอรายละเอียดเพิ่มเติ่ม: ")
             d = dd(arg2, temp)
         if isinstance(d, str):
             return d
         else:
-            temp = d
-            d = dd(word,d)
+            if isinstance(d,dict):
+                d = dd(word,d)
 if __name__ == '__main__':
-    print(check("tin"))
+    x = input("มีปัญหาเกี่ยวกับอะไร : ")
+    print(check(x,dict_target))
