2a185ae75ca2a613894e42106dd6ceee43665934
tin537 <arekorlab@gmail.com>
2017-06-03 12:03:21 +0700

    t0002 if

--- train/ifst.py
+++ train/ifst.py
@@ -2,7 +2,15 @@ a = 1
 b = 2
 if not(a == 1)or(b == 2):
     print(True)
-elif(b==2 and a ==0):
+elif b==2 and a ==0:
     print(False)
 else:
     print("hi")
+'''
+This is a multiline
+comment.
+'''
+#test
+str = "hidsffsdvklfbs"
+if "hi" in str:
+    print("have")
