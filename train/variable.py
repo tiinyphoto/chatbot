cef7106da0bacff11a14a764e4bef54573147304
tin537 <arekorlab@gmail.com>
2017-06-03 11:14:51 +0700

    t0001

---
+++ train/variable.py
@@ -0,0 +1,31 @@
+a = 1
+b = 0.4
+c = 'a'
+cc = "aaa"
+hh = ['tt','yy','yyy',777]
+jj = {'gg','tt','55',4}
+ii = {"1":"dog",'2':3}
+print("a = ",a," type ",type(a))
+print("b = ",b," type ",type(b))
+print("c = ",c," type ",type(c))
+print("cc = ",cc," type ",type(cc))
+print("list of cc")
+for i in  cc :
+    print(i)
+print("hh = ",hh," type ",type(hh))
+print("list of hh")
+for i in  hh :
+    print(i)
+print("jj = ",jj," type ",type(jj))
+print("list of jj")
+for i in  jj :
+    print(i)
+print("ii = ",ii," type ",type(ii))
+print("list of ii")
+for i in  ii :
+    print(i," ",ii[i])
+
+x,y = "blue","green"
+print (x , y )
+x,y,z = "aaa"
+print (x , y , z)
